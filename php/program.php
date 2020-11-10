561c2ea0317d5f9f52d81d1299f40d65cb3cd833
Josep Sanz <josep.sanz@saltos.org>
2020-11-10 19:33:00 +0100

    Mes millores al interface

--- php/program.php
+++ php/program.php
@@ -4,7 +4,8 @@ include("php/functions.php");
 
 // PREPARE FILES LIST
 $files=glob("files/*.ly");
-$array=array();
+$cats=array();
+$songs=array();
 $count1=0;
 $count2=0;
 foreach($files as $file) {
@@ -14,20 +15,35 @@ foreach($files as $file) {
 	$cat2=ucfirst(str_replace("_"," ",$cat));
 	$song2=ucfirst(str_replace("_"," ",$song));
 	$info=implode(" - ",metadata($file));
-	if(!isset($array[$cat])) {
-		$array[$cat]=array("name"=>$cat2,"array"=>array());
+	if(!isset($cats[$cat])) {
+		$cats[$cat]=array("name"=>$cat2,"array"=>array());
 		$count1++;
 	}
-	$array[$cat]["array"][$song]=array("name"=>$song2,"info"=>$info,"hash"=>$hash);
+	$cats[$cat]["array"][]=$hash;
+	$songs[$hash]=array(
+		"name"=>$song2,
+		"info"=>$info,
+		"ly"=>glob("files/${hash}.ly"),
+		"pdf"=>glob("files/${hash}.pdf"),
+		"midi"=>array_merge(glob("files/${hash}.midi"),glob("files/${hash}-*.midi")),
+		"mp3"=>array_merge(glob("files/${hash}.mp3"),glob("files/${hash}-*.mp3")),
+	);
 	$count2++;
 }
 
 // MAKE TITLE
 $title="${count2} partitures i cançons per a gralla en ${count1} gèneres";
 
+// MAKE LABELS
+$labels=array(
+	"ly"=>"Fitxer font per Lilypond",
+	"pdf"=>"Fitxer de document PDF",
+	"midi"=>"Fitxer de seqüència MIDI",
+	"mp3"=>"Fitxer de so MP3",
+);
+
 // PREPARE TEMPLATE
 $template=file_get_contents("template/index.html");
-$template=str_replace(array("img/","css/","js/"),array("template/img/","template/css/","template/js/"),$template);
 $template=explode("<!-- ROWROWROW -->",$template);
 
 $html=array();
@@ -36,7 +52,13 @@ $html[]=str_replace(
 	array($title,"Cercar"),
 	$template[0]);
 
-$json=json_encode(array("title"=>$title,"array"=>$array,"template"=>array_slice($template,1,3)));
+$json=json_encode(array(
+	"title"=>$title,
+	"labels"=>$labels,
+	"cats"=>$cats,
+	"songs"=>$songs,
+	"template"=>array_slice($template,1,3),
+));
 $html[]="<script>var data=${json}</script>";
 
 $html[]=$template[4];
