b51fb9794a57cc1e8871dc9713ceca92da073622
Josep Sanz <josep.sanz@saltos.org>
2020-11-15 22:24:34 +0100

    Optimitzar la carrega de js i css serialitzant-ho tot en un unic fitxer js i css

--- php/index.php
+++ php/index.php
@@ -83,6 +83,43 @@ $html[]="<script>var data=${json}</script>";
 $html[]=$template[9];
 foreach($html as $key=>$val) $html[$key]=trim($val,"\n");
 $html=implode("\n",$html);
+
+$html=explode("\n",$html);
+$css=array();
+$js=array();
+foreach($html as $key=>$val) {
+	$val=trim($val);
+	$pos1=strpos($val,'<script src="');
+	$pos2=strpos($val,'"></script>');
+	$pos3=strpos($val,'<link href="');
+	$pos4=strpos($val,'" rel="stylesheet">');
+	if($pos1!==false && $pos2!==false) {
+		$len1=strlen('<script src="');
+		$js[]=substr($val,$pos1+$len1,$pos2-$pos1-$len1);
+		unset($html[$key]);
+	} elseif($pos3!==false && $pos4!==false) {
+		$len3=strlen('<link href="');
+		$css[]=substr($val,$pos3+$len3,$pos4-$pos3-$len3);
+		unset($html[$key]);
+	} elseif($val=="</head>") {
+		$val='<link href="lib/all.min.css" rel="stylesheet">'.$val;
+		$html[$key]=$val;
+	} elseif($val=="</body>") {
+		$val='<script src="lib/all.min.js"></script>'.$val;
+		$html[$key]=$val;
+	}
+}
+$html=implode("\n",$html);
 file_put_contents("index.html",$html);
+
+$data=array();
+foreach($css as $file) $data[]=str_replace("images/","pdfjs/images/",file_get_contents($file));
+$data=implode("\n",$data);
+file_put_contents("lib/all.min.css",$data);
+
+$data=array();
+foreach($js as $file) $data[]=file_get_contents($file);
+$data=implode("\n",$data);
+file_put_contents("lib/all.min.js",$data);
 
 ?>
