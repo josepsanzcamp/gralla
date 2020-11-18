9e893e9568352c7e7b1c87cf25762b8f23ca8143
Josep Sanz <josep.sanz@saltos.org>
2020-11-18 15:31:44 +0100

    Afegir les traduccions a castellà i anglès

--- php/program.php
+++ php/program.php
@@ -1,8 +1,17 @@
 <?php
 
 include("php/functions.php");
+foreach(array("ca","es","en") as $lang) {
+	include("php/langs.php");
+	$labels=$labels[$lang];
+	include("php/index.php");
+	rename("index.html","index.${lang}.html");
+}
+
+copy("template/index.js","index.html");
+
 include("php/langs.php");
-include("php/index.php");
+$labels=$labels["ca"];
 include("php/sitemap.php");
 
 ?>
