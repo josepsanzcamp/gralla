eb91932919259cc4aab8c01a232716867a926db7
Josep Sanz <josep.sanz@saltos.org>
2020-11-26 07:16:02 +0100

    Afegir una minimitzacio de js molt simple

--- php/minify.php
+++ php/minify.php
@@ -3,7 +3,7 @@
 function html_minify($data) {
 	$data=str_replace(array("\n","\r","\t")," ",$data);
 	for($i=0;$i<100;$i++) $data=str_replace("  "," ",$data);
-	$data=str_replace("> <","><",$data);
+	$data=trim(str_replace("> <","><",$data));
 	return $data;
 }
 
@@ -11,6 +11,22 @@ function css_minify($data) {
 	$data=str_replace(array("\n","\r","\t"),"",$data);
 	for($i=0;$i<100;$i++) $data=str_replace("  "," ",$data);
 	foreach(array(":",";","{","}",",") as $temp) $data=str_replace(array(" ".$temp." "," ".$temp,$temp." "),$temp,$data);
+	$data=trim($data);
+	return $data;
+}
+
+function js_minify($data) {
+	$data=explode("\n",$data);
+	foreach($data as $key=>$val) {
+		$val=trim($val);
+		$pos1=strpos($val,"//");
+		$pos2=strpos($val,"/*");
+		$len=strlen($val);
+		if($pos1!==false || $pos2!==false || $len>200) $val="\n".$val."\n";
+		$data[$key]=$val;
+	}
+	$data=implode("",$data);
+	$data=trim(str_replace("\n\n","\n",$data));
 	return $data;
 }
 
