b48d620eac93249657b82f6de3c8c435f5ffa641
Josep Sanz <josep.sanz@saltos.org>
2020-11-26 17:35:53 +0100

    Optimitzar una tonteria sense importancia

--- php/minify.php
+++ php/minify.php
@@ -2,14 +2,14 @@
 
 function html_minify($data) {
 	$data=str_replace(array("\n","\r","\t")," ",$data);
-	for($i=0;$i<100;$i++) $data=str_replace("  "," ",$data);
+	do $data=str_replace("  "," ",$data,$count); while($count);
 	$data=trim(str_replace("> <","><",$data));
 	return $data;
 }
 
 function css_minify($data) {
 	$data=str_replace(array("\n","\r","\t"),"",$data);
-	for($i=0;$i<100;$i++) $data=str_replace("  "," ",$data);
+	do $data=str_replace("  "," ",$data,$count); while($count);
 	foreach(array(":",";","{","}",",") as $temp) $data=str_replace(array(" ".$temp." "," ".$temp,$temp." "),$temp,$data);
 	$data=trim($data);
 	return $data;
