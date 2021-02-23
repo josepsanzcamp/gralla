3cd155072174a6d4d34134628c66524da152b0dd
Josep Sanz <josep.sanz@saltos.org>
2021-02-23 22:55:11 +0100

    Fer servir el hash com a nom del fitxer static per compatibilitat amb el fitxer lilypond original

--- php/static.php
+++ php/static.php
@@ -9,14 +9,13 @@ foreach($dirs as $dir) {
 	$info2=implode(" - ",$info);
 	$info3=implode(", ",$info);
 	$info4=mb_strtolower($info3);
-	$info5=encode_bad_chars($info3,"-");
 	$data=file_get_contents("template/static.html");
 	$data=str_replace(
 		array("__TITLE__","__DESCRIPTION__","__AUTHOR__","__KEYWORDS__","__LANG__","__HASH__"),
 		array($info2." - ".$labels["title"],$info3.", ".$labels["description"],$labels["author"],$info4.", ".$labels["keywords"],$lang,$hash),
 		$data);
 	$data=html_minify($data);
-	file_put_contents("static/${info5}.${lang}.html",$data);
+	file_put_contents("static/${hash}.${lang}.html",$data);
 
 }
 
