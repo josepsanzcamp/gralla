d1e2e5203595f09a0e356cf1a383cf54ee847ad0
Josep Sanz <josep.sanz@saltos.org>
2022-11-01 17:31:59 +0100

    Mes millores als scripts de php i js

--- php/index.php
+++ php/index.php
@@ -117,14 +117,6 @@ foreach ($html as $key => $val) {
 }
 $html = implode("\n", $html);
 
-# FOR DEBUG PURPOSES ONLY
-$debug = 0;
-if ($debug) {
-    $html = str_replace("<head>", "<head><base href='./index.${lang}.html'/>", $html);
-    file_put_contents("index.${lang}.html", $html);
-    die();
-}
-
 // SAVE ALL OTHER FILES
 list($html,$js,$css) = html_minify2($html);
 $html = html_minify($html);
